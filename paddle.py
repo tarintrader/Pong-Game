8b4ace3e201991c1155c5f95636e74a689ecaf1c
Adrian <tarintrader@gmail.com>
2024-02-21 19:33:35 +0000

    Initial commit

---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.setposition(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.sety(new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.sety(new_y)
